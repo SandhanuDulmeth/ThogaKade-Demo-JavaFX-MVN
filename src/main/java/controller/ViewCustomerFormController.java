b07b19602dc85db9f8a185f26e6e6e484765c625
SandhanuDulmeth <sandhanu12@gmail.com>
2024-12-19 11:52:35 +0530

    Commit

--- src/main/java/controller/ViewCustomerFormController.java
+++ src/main/java/controller/ViewCustomerFormController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
 import db.DBConnection;
 import javafx.beans.property.Property;
 import javafx.collections.FXCollections;
@@ -7,19 +8,25 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import model.Customer;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ViewCustomerFormController implements Initializable {
 
-    public Label lblId;
+
+    public JFXTextField txtid;
+
+
     @FXML
     private TableColumn<?, ?> colAddress;
 
@@ -34,27 +41,44 @@ public class ViewCustomerFormController implements Initializable {
 
     @FXML
     private TableView<Customer> tblCus;
-
+     List<Customer> customerList= DBConnection.getINSTANCE().getConnection();
     @FXML
     void btnReloadOnAction(ActionEvent event) {
-        colId.setCellValueFactory(new PropertyValueFactory<>("Id"));
-        colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
-        colAddress.setCellValueFactory(new PropertyValueFactory<>("Address"));
-        colSalary.setCellValueFactory(new PropertyValueFactory<>("Salary"));
-        List<Customer> customerList= DBConnection.getINSTANCE().getConnection();
-        ObservableList<Customer> objects = FXCollections.observableArrayList();
-        customerList.forEach(obj->
-                objects.add(obj));
-        tblCus.setItems(objects);
+        loardTable();
 
     }
+public void loardTable(){
+    colId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+    colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+    colAddress.setCellValueFactory(new PropertyValueFactory<>("Address"));
+    colSalary.setCellValueFactory(new PropertyValueFactory<>("Salary"));
 
+    ObservableList<Customer> objects = FXCollections.observableArrayList();
+    customerList.forEach(obj->objects.add(obj));
+    tblCus.setItems(objects);
+
+}
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tblCus.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) ->{
-            System.out.println(newValue);
-            lblId.setText(newValue.getId());
+
+            setValues(newValue);
         } );
 
+    }
+    private void setValues(Customer customer){
+
+        txtid.setText(customer.getId());
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+        String id = txtid.getText();
+        customerList.forEach(customer -> {
+            if (customer.getId().equals(id)){
+                customer.setId(id);
+            }
+        });
+        loardTable();
     }
 }
